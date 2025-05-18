87b5eda25e9b2be780a5fb25709efbf3e0315a9a
HanZawNyein <hanzawnyineonline@gmail.com>
2025-05-18 14:25:43 +0630

    [ADD] menu

--- addons/technortal_hms/__manifest__.py
+++ addons/technortal_hms/__manifest__.py
@@ -2,4 +2,9 @@
     "name":"Hotel Management System",
     "author":"Agga, IdeaCode Academy",
     "license":"LGPL-3",
+    "data":[
+        "views/hms_hotel.xml",
+
+        "views/menus.xml",
+    ]
 }
