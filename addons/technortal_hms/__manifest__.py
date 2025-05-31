e09a789460604bfda2bf80228c7437bac91b1dab
HanZawNyein <hanzawnyineonline@gmail.com>
2025-05-31 14:13:38 +0630

    [ADD] sequence

--- addons/technortal_hms/__manifest__.py
+++ addons/technortal_hms/__manifest__.py
@@ -4,6 +4,7 @@
     "license":"LGPL-3",
     "depends":["contacts"],
     "data":[
+        "data/sequence.xml",
         "security/ir.model.access.csv",
 
         "views/hms_hotel.xml",
--- addons/technortal_hms/models/hms_booking.py
+++ addons/technortal_hms/models/hms_booking.py
@@ -4,8 +4,9 @@ from odoo import api, fields, models,_
 class HmsBooking(models.Model):
     _name = 'hms.booking'
     _description = 'HmsBooking'
+    _rec_name = 'reference'
 
-    reference = fields.Char( default=lambda self: _('New'),readonly=True)
+    reference = fields.Char( default=lambda self: _('New'),readonly=True,copy=False)
     description = fields.Text()
     hotel_id = fields.Many2one('hms.hotel', required=True)
     room_id = fields.Many2one('hms.room', required=True)
@@ -19,6 +20,14 @@ class HmsBooking(models.Model):
          ('paid', 'Paid'),
          ('done', 'Done'),
          ('cancel', 'Cancelled')], default='draft')
+
+    @api.model
+    def create(self, values):
+        # Add code here
+        # values['reference']='B10000000'
+        if (not values.get('reference')) or values.get('reference') == _('New'):
+            values['reference'] = self.env['ir.sequence'].next_by_code('hms.booking') or _('New')
+        return super(HmsBooking, self).create(values)
 
     def action_draft(self):
         self.state = 'draft'
