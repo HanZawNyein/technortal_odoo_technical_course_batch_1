de19e3cbde64d457604b0a79bdc7db54e6a426ac
HanZawNyein <hanzawnyineonline@gmail.com>
2025-06-01 13:32:05 +0630

    [ADD] db level constrains & app level constrains

--- addons/technortal_hms/models/hms_room.py
+++ addons/technortal_hms/models/hms_room.py
@@ -1,4 +1,5 @@
-from odoo import api, fields, models
+from odoo import api, fields, models,_
+from odoo.exceptions import ValidationError
 
 
 class HmsRoom(models.Model):
@@ -6,17 +7,27 @@ class HmsRoom(models.Model):
     _description = 'HmsRoom'
 
     name = fields.Char(required=True)
-    hotel_id = fields.Many2one('hms.hotel',required=True)
+    hotel_id = fields.Many2one('hms.hotel', required=True)
     type = fields.Selection([
         ('normal', 'Normal'),
         ('vip', 'VIP'),
-        ('vvip','VVIP')],default='normal')
+        ('vvip', 'VVIP')], default='normal')
+
+    _sql_constraints = [
+        ('hms_room_name_unique', 'unique(name)', 'Room name already exists.')
+    ]
 
     def action_view_booking(self):
         return {
-            "type":"ir.actions.act_window",
-            "name":f"{self.name}'s booking",
-            "view_mode":"list,form",
-            "res_model":"hms.booking",
-            "domain":[('room_id','=',self.id),('hotel_id','=',self.hotel_id.id)]
+            "type": "ir.actions.act_window",
+            "name": f"{self.name}'s booking",
+            "view_mode": "list,form",
+            "res_model": "hms.booking",
+            "domain": [('room_id', '=', self.id), ('hotel_id', '=', self.hotel_id.id)]
         }
+
+
+    # @api.constrains('name')
+    # def _check_name(self):
+    #     if self.name in self.search([('id','!=',self.id)]).mapped('name'):
+    #         raise ValidationError(_("Name should be unique."))
