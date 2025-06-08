11b02fa1f151371bafc8a1ddf46a760d53e574a0
HanZawNyein <hanzawnyineonline@gmail.com>
2025-06-08 14:44:35 +0630

    [ADD] inherit/modify existing models

--- addons/technortal_hms/__manifest__.py
+++ addons/technortal_hms/__manifest__.py
@@ -2,10 +2,11 @@
     "name":"Hotel Management System",
     "author":"Agga, IdeaCode Academy",
     "license":"LGPL-3",
-    "depends":["contacts","mail"],
+    "depends":["base","contacts","mail"],
     "data":[
         "data/sequence.xml",
         "security/ir.model.access.csv",
+        "views/res_partner.xml",
 
         "views/hms_hotel.xml",
         "views/hms_room.xml",
--- addons/technortal_hms/models/__init__.py
+++ addons/technortal_hms/models/__init__.py
@@ -1,3 +1,4 @@
 from . import hms_hotel
 from . import hms_room
 from . import hms_booking
+from . import res_partner
