806ce796bf4edd9d6f5866645c86110514127ebd
HanZawNyein <hanzawnyineonline@gmail.com>
2025-07-20 13:15:05 +0630

    [ADD] client action

--- addons/technortal_hms/static/src/sample_client_action/sample_client_action.js
+++ addons/technortal_hms/static/src/sample_client_action/sample_client_action.js
@@ -1,9 +1,40 @@
-import { registry } from "@web/core/registry";
+import {registry} from "@web/core/registry";
 
-import { Component } from  "@odoo/owl";
+import {Component, useRef,useState,onWillStart} from "@odoo/owl";
+import {useService} from "@web/core/utils/hooks";
 
 class SampleClientAction extends Component {
     static template = "technortal_hms.SampleClientAction";
+
+    setup() {
+        this.searchRef = useRef('search-input');
+        this.state = useState({
+            partners: [
+                // {id: 1, name: 'A'},
+                // {id: 2, name: 'B'},
+            ]
+        })
+        this.ormService = useService('orm');
+        onWillStart(async ()=>{
+            await this.getAllPartners()
+        })
+    }
+
+    async getAllPartners() {
+        this.state.partners = await this.ormService.searchRead('res.partner', [], ['id', 'display_name'])
+    }
+
+    async search() {
+        // console.log("hrllo")
+        // console.log()
+        let data = this.searchRef.el.value;
+        if(data){
+            this.state.partners = this.state.partners.filter(partner=>partner.display_name === data)
+        }else {
+            await  this.getAllPartners();
+        }
+        this.searchRef.el.value = null;
+    }
 }
 
 // remember the tag name we put in the first step
